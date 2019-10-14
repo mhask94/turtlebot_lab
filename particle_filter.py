3cb674d101340db7fc4db65935359237d4b19c3e
Mat Haskell <mhaskell9@gmail.com>
2019-10-14 10:52:50 -0600

    Refactored motion model

--- particle_filter.py
+++ particle_filter.py
@@ -5,11 +5,10 @@ from utils import wrap, rand, MotionModel, MeasurementModel
 class ParticleFilter():
     def __init__(self, alphas, Q, num_particles=1000, limits=[-10,10,-10,10],
             landmarks=np.empty(0)):
-        self.a1, self.a2, self.a3, self.a4 = alphas
         self.Q = Q.diagonal().reshape((len(Q),1))
         self.M = num_particles
         self.landmarks = landmarks
-        self.g = MotionModel()
+        self.g = MotionModel(alphas, noise=True)
         self.h = MeasurementModel()
 
         self.chi = np.empty((4,num_particles))
@@ -36,14 +35,8 @@ class ParticleFilter():
         self.chi = self.chi[:,i]
 
     def predictionStep(self, u, dt): # u is np.array size 2x1
-        # add noise to commanded inputs
-        u_noisy = np.zeros((len(u), self.M))
-        vsig = np.sqrt(self.a1*u[0]**2 + self.a2*u[1]**2)
-        wsig = np.sqrt(self.a3*u[0]**2 + self.a4*u[1]**2)
-        u_noisy[0] = u[0] + vsig*randn(self.M)
-        u_noisy[1] = u[1] + wsig*randn(self.M)
         # propagate dynamics through motion model
-        self.chi[:3] = self.g(u_noisy, self.chi[:3], dt)
+        self.chi[:3] = self.g(u, self.chi[:3], dt)
         # update mu
         self.mu = np.mean(self.chi[:3], axis=1, keepdims=True)
 
--- utils.py
+++ utils.py
@@ -11,29 +11,45 @@ def wrap(angle, dim=None):
 def rand(size=(), min_=0, max_=1):
     return min_ + np.random.rand(*size)*(max_ - min_)
 
+
 class MotionModel():
-    def __init__(self):
-        pass
+    def __init__(self, alphas, noise=True):
+        self.a1, self.a2, self.a3, self.a4 = alphas[:4]
+        if len(alphas) == 6:
+            self.a5, self.a6 = alphas[4:6]
+        else:
+            self.a5, self.a6 = 0, 0
+        self.noise = noise
 
     def __call__(self, u, x_m1, dt):
-        n0, = np.where(u[1] != 0)    # non-zero indices of omega
-        vhat = u[0] 
-        what = u[1] 
+        # add noise if needed
+        u_noisy = np.zeros((len(u), len(x_m1[0])))
+        vsig = np.sqrt(self.a1*u[0]**2 + self.a2*u[1]**2) * self.noise
+        wsig = np.sqrt(self.a3*u[0]**2 + self.a4*u[1]**2) * self.noise
+        gamsig = np.sqrt(self.a5*u[0]**2 + self.a6*u[1]**2) * self.noise
+        u_noisy[0] = u[0] + vsig*randn(len(x_m1[0]))
+        u_noisy[1] = u[1] + wsig*randn(len(x_m1[1]))
+
+        n0, = np.where(u_noisy[1] != 0)    # non-zero indices of omega
+        vhat = u_noisy[0] 
+        what = u_noisy[1] 
         temp = vhat[n0] / what[n0] 
         w_dt = what * dt
+        gamma_dt = gamsig*randn(len(w_dt)) * dt
         theta = x_m1[2][n0]
 
         x = np.zeros(x_m1.shape)
         x[0][n0] = x_m1[0][n0] + temp*(np.sin(theta+w_dt[n0])-np.sin(theta))
         x[1][n0] = x_m1[1][n0] + temp*(np.cos(theta)-np.cos(theta+w_dt[n0]))
-        x[2] = wrap(x_m1[2] + w_dt)
+        x[2] = wrap(x_m1[2] + w_dt + gamma_dt)
 
-        if len(n0) != len(u[1]): 
-            y0, = np.where(u[1] == 0) # zero indices of omega
+        if len(n0) != len(u_noisy[1]): 
+            y0, = np.where(u_noisy[1] == 0) # zero indices of omega
             theta = x_m1[2][y0]
             x[0][y0] = x_m1[0][y0] + vhat[y0]*dt*np.cos(theta)
             x[1][y0] = x_m1[1][y0] + vhat[y0]*dt*np.sin(theta)
         return x
+
 
 class MeasurementModel():
     def __init__(self):
